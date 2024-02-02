588fae4b04abdd72a715ef514ac62c73bd3ad6ed
Manashjyoti Boro <manashjyoti533@gmail.com>
2024-02-02 23:35:33 +0530

    update: memberCart users

--- src/pages/MemberCart.tsx
+++ src/pages/MemberCart.tsx
@@ -1,23 +1,27 @@
+import { users } from "../utils/dummyData"
 
 const MemberCart = () => {
 return (
-    <div className="h-screen bg-slate-900  flex justify-center items-center shadow-lg">
-        <div className="bg-white h-[350px] w-[400px] rounded-xl relative">
+    <div className="bg-slate-900 ">
+        <h1 className="text-white pt-40 text-5xl text-center font-bold tracking-wider">TEAM MEMBERS</h1>
+    <div className="h-screen  flex justify-center items-center shadow-lg gap-10 -mt-40">
+        {users.map((user) => (
+            <div className="bg-white h-[350px] w-[400px] rounded-xl relative">
         {/* Img cover */}
         <div className="">
-                <img className="h-[90px] w-full object-cover object-center rounded-t-xl" src="https://images.unsplash.com/photo-1596226799589-ea489243e102?q=80&w=1992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+                    <img className="h-[90px] w-full object-cover object-center rounded-t-xl" src={user.imgCover} />
         </div>
         
         {/* content */}
         <div>
             <div>
-                <img className="h-20 rounded-full absolute top-14 left-40 border-2 border-white" src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" />
+                <img className="h-20 rounded-full absolute top-14 left-40 border-2 border-white" src={user.imgProfile} />
             </div>
 
             <div className="mt-14 text-center font-semibold">
-                <h1 className="text-xl">Manashjyoti Boro</h1>
-                <h1 className="text-[#6d6d6d] text-md">Web Developer</h1>
-                <p className="text-[#6d6d6d] text-md pt-2">Web developer based in India<br/>passionate about developing websites </p>
+                <h1 className="text-xl">{user.name} </h1>
+                <h1 className="text-[#6d6d6d] text-md">{user.position} </h1>
+                        <p className="text-[#6d6d6d] text-md pt-2 w-4/5 m-auto">{user.desc}</p>
                 <div className="flex justify-evenly w-full  mt-6">
                     <div className="bg-indigo-600 text-white w-2/5 p-2 rounded-lg cursor-pointer">Follow</div>
                     <div className="text-[#6d6d6d] border border-[#6d6d6d] w-2/5 p-2 rounded-lg cursor-pointer">Message</div>
@@ -25,7 +29,10 @@ return (
             </div>
         </div>
         </div>
-    </div>
+        ))}
+        
+        </div>
+        </div>
     )
 }
 
---
+++ src/utils/dummyData.tsx
@@ -0,0 +1,23 @@
+export const users = [
+    {
+        name: "Manashjyoti Boro",
+        position: "Web Developer",
+        desc: "Web developer based in India passionate about developing websites ",
+        imgProfile:"https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp",
+        imgCover:"https://images.unsplash.com/photo-1596226799589-ea489243e102?q=80&w=1992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Second Member",
+        position: "UI/UX Designer",
+        desc: "UX/UI designer based in India passionate about designing websites ",
+        imgProfile:"https://static.vecteezy.com/system/resources/previews/019/896/012/original/female-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png",
+        imgCover:"https://images.unsplash.com/photo-1596226799589-ea489243e102?q=80&w=1992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Third Member",
+        position: "Tester",
+        desc: "Web tester based in India passionate about testing websites performance",
+        imgProfile:"https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png",
+        imgCover:"https://images.unsplash.com/photo-1596226799589-ea489243e102?q=80&w=1992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+]
